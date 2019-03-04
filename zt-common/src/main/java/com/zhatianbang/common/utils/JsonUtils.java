a6b1efae332770811a37b7cb66f4cd178bd1568f
jingfei <743793426@qq.com>
2019-03-04 14:46:42 +0800

    删除nullable

--- zt-common/src/main/java/com/zhatianbang/common/utils/JsonUtils.java
+++ zt-common/src/main/java/com/zhatianbang/common/utils/JsonUtils.java
@@ -3,7 +3,6 @@ package com.zhatianbang.common.utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.istack.internal.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,7 +20,6 @@ public class JsonUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(JsonUtils.class);
 
-    @Nullable
     public static String serialize(Object obj) {
         if (obj == null) {
             return null;
@@ -37,7 +35,6 @@ public class JsonUtils {
         }
     }
 
-    @Nullable
     public static <T> T parse(String json, Class<T> tClass) {
         try {
             return mapper.readValue(json, tClass);
@@ -47,7 +44,6 @@ public class JsonUtils {
         }
     }
 
-    @Nullable
     public static <E> List<E> parseList(String json, Class<E> eClass) {
         try {
             return mapper.readValue(json, mapper.getTypeFactory().constructCollectionType(List.class, eClass));
@@ -57,7 +53,6 @@ public class JsonUtils {
         }
     }
 
-    @Nullable
     public static <K, V> Map<K, V> parseMap(String json, Class<K> kClass, Class<V> vClass) {
         try {
             return mapper.readValue(json, mapper.getTypeFactory().constructMapType(Map.class, kClass, vClass));
@@ -67,7 +62,6 @@ public class JsonUtils {
         }
     }
 
-    @Nullable
     public static <T> T nativeRead(String json, TypeReference<T> type) {
         try {
             return mapper.readValue(json, type);
