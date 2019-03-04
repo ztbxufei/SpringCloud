66284388d2a37cf5f0b29d72f9489c95c18c3436
xufei <18855974469@163.com>
2019-03-04 14:53:07 +0800

    新建子模块zt-user

---
+++ zt-user/zt-user-service/src/main/java/com/zhatianbang/user/ZtUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zhatianbang.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZtUserServiceApplication.class, args);
+	}
+
+}
