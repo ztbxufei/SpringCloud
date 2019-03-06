acfedf2209315d6b7eb923c7614e411e5fa8cba5
jingfei <743793426@qq.com>
2019-03-06 15:26:07 +0800

    pom文件修改

--- zt-gateway/src/main/java/com/zhatianbang/ZtGatewayApplication.java
+++ zt-gateway/src/main/java/com/zhatianbang/ZtGatewayApplication.java
@@ -2,8 +2,12 @@ package com.zhatianbang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class ZtGatewayApplication {
 
 	public static void main(String[] args) {
